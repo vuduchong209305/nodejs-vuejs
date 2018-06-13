636e515048d30762b6c2f432ddfe366161360531
vuduchong <vuduchong209305@gmail.com>
2018-06-13 20:26:12 +0700

    edit code

--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -1,36 +1,94 @@
 const express = require('express')
 const router = express.Router()
 
+const Product = require('./../models/product')
+
 router.get('/', (req, res, next) => {
-    res.json({
-        message : 'Method GET'
-    })
+    Product.find()
+        .exec()
+        .then(result => {
+            console.log(result)
+            if(result.length > 0) {
+                res.status(200).json(result)
+            } else {
+                res.status(404).json({
+                    message : 'Not found'
+                })
+            }
+        })
 })
 
 router.post('/', (req, res, next) => {
-    res.json({
-        message : 'Method POST'
+    const product = new Product({
+        name : req.body.name,
+        price : req.body.price
     })
+    product.save()
+        .then(result => {
+            console.log(result)
+            res.json({
+                message : 'Product was created',
+                result : result
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.json({
+                error: err
+            })
+        })
+    
 })
 
 router.get('/:productID', (req, res, next) => {
     const id = req.params.productID
-    if(id === 'special') {
-        res.json({
-            message: 'You discovered the spacial productID',
-            id : id
+    Product.findById(id).exec()
+        .then(result => {
+            console.log(result)
+            res.json(result)
         })
-    } else {
-        res.json({
-            message: 'You passed an ID'
+        .catch(err => {
+            console.log(err)
+            res.json({
+                error: err
+            })
         })
+
+})
+
+router.patch('/:productID', (req, res, next) => {
+    const id = req.params.productID
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
     }
+    Product.update({ _id : id }, { $set: updateOps })
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 router.delete('/:productID', (req, res, next) => {
-    res.json({
-        message : 'Method DELETE'
-    })
+    const id = req.params.productID
+    Product.remove({ _id : id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 })
 
 module.exports = router
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,37 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const productRoutes = require('./../api/routes/products.js')
 
+mongoose.connect('mongodb://localhost:27017/mongodb').then(
+    () => {
+        console.log('Connect MongoDB successfully')
+    },
+    err => {
+        console.log(`Connect faild, error : ${err}`)
+    }
+)
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.use('/products', productRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
